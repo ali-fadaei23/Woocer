6bba4e405569acfab3d5ba4528ce61237a58edb6
Ali Fadaei <razorn1375@gmail.com>
2021-04-20 22:23:49 +0430

    fix styles of features component

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -6,18 +6,34 @@ import "./Features.css";
 import Forward from "../Assets/forward.png";
 
 const BtnFeature = (props) => (
-  <div style={{ color: "#7F83FC" }}>
-    <div style={{ borderRadius: "50%", color: "#767AF7", marginRight: "5px" }}>
+  <div
+    style={{
+      backgroundColor: "#7F83FC",
+      marginBottom: "10%",
+      display: "flex",
+      padding: "8px",
+      borderRadius: "10px",
+    }}
+  >
+    <div className="features-box">
       <img
         src={Forward}
-        style={{ filter: "brightness(0) invert(1)", width: "50px" }}
+        style={{
+          filter: "brightness(0) invert(1)",
+          width: "40px",
+        }}
       />
-      <button
-        style={{ backgroundColor: "transparent", border: 0, color: "#fff" }}
-      >
-        {props.text}
-      </button>
     </div>
+    <button
+      style={{
+        backgroundColor: "transparent",
+        border: 0,
+        color: "#fff",
+        padding: "20px",
+      }}
+    >
+      {props.text}
+    </button>
   </div>
 );
 
@@ -29,34 +45,34 @@ const Features = () => (
       fontSize: "20px",
     }}
   >
-    <div style={{ padding: "20px", fontFamily: "Poppins" }}>
+    <div style={{ padding: "60px", fontFamily: "Poppins" }}>
       <h2 style={{ fontSize: "40px" }}>Some of our features</h2>
     </div>
-    <div style={{ textAlign: "left", padding: "50px" }}>
-      <Row>
-        <Col>
-          <BtnFeature text={"Manage Product"} />
 
-          <BtnFeature text={"Manage Reviews"} />
+    <Row>
+      <Col>
+        <BtnFeature text={"Manage Product"} />
 
-          <BtnFeature text={"Login with fingerprint"} />
-        </Col>
-        <Col>
-          <BtnFeature text={"Manage Orders"} />
+        <BtnFeature text={"Manage Reviews"} />
 
-          <BtnFeature text={"Manage Tags"} />
+        <BtnFeature text={"Login with fingerprint"} />
+      </Col>
+      <Col>
+        <BtnFeature text={"Manage Orders"} />
 
-          <BtnFeature text={"Customizable Profile"} />
-        </Col>
-        <Col>
-          <BtnFeature text={"Manage Customers"} />
+        <BtnFeature text={"Manage Tags"} />
 
-          <BtnFeature text={"Manage Categories"} />
+        <BtnFeature text={"Customizable Profile"} />
+      </Col>
+      <Col>
+        <BtnFeature text={"Manage Customers"} />
 
-          <BtnFeature text={"And so much more..."} />
-        </Col>
-      </Row>
-    </div>
+        <BtnFeature text={"Manage Categories"} />
+
+        <BtnFeature text={"And so much more..."} />
+      </Col>
+    </Row>
+    <div className="space-between-features" style={{ padding: "40px" }}></div>
   </Container>
 );
 
