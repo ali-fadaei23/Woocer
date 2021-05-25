83450a1cd8db9147955731bce051b63112437b9d
Ali Fadaei <razorn1375@gmail.com>
2021-05-25 12:11:55 +0430

    add img animation in about component

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,27 +6,34 @@ import "./About.css";
 import ImageAbout from "../Assets/Woocer-screenshot-2.png";
 
 const About = () => (
-  <Container style={{ maxWidth: "100%", color: "#000", fontSize: "20px" }}>
+  <Container
+    style={{
+      maxWidth: "100%",
+      color: "#000",
+      fontSize: "20px",
+      padding: "60px",
+    }}
+  >
     <Row>
       <Col>
-        <div>
-          <img src={ImageAbout} />
+        <div className="box-img-about">
+          <img className="img-about" src={ImageAbout} />
         </div>
       </Col>
       <Col>
         <div className="box-title-about">
-          <div className="box-text-about">
+          <div className="box-main-text-about">
             <div>
               <span>Manage your products</span>
             </div>
-            <div>
-              <span>
-                The href attribute requires a valid value to be accessible.
+            <div className="box-text-about">
+              <span className="text-about" placeholder="Text">
+                Woocer let's you manage your store from your mobile device.
               </span>
             </div>
           </div>
           <div className="box-btn-about">
-            <Button>Select</Button>
+            <Button className="btn-about">Manage Product</Button>
           </div>
         </div>
       </Col>
