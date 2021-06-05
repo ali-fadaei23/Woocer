63e7e6ebe1f1f75f6715ce626ee992896dcdea19
Ali Fadaei <razorn1375@gmail.com>
2021-06-05 17:33:35 +0430

    Set position elements in footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,16 +2,91 @@ import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import "./Footer.css";
 
+import Cloud1 from "../Assets/icon-cloud-1.png";
+import Cloud2 from "../Assets/icon-cloud-2.png";
+import Cloud3 from "../Assets/icon-cloud-3.png";
+import Cloud4 from "../Assets/icon-cloud-4.png";
+import Cloud5 from "../Assets/icon-cloud-2.png";
+import Cloud6 from "../Assets/icon-cloud-4.png";
+import IconBuilding from "../Assets/icon-buildings.png";
+
 const Footer = () => (
-  <Container style={{ maxWidth: "100%", color: "#fff", fontSize: "20px" }}>
+  <Container
+    style={{
+      maxWidth: "100%",
+      position: "relative",
+      backgroundColor: "#fcfafb",
+      color: "#394963",
+    }}
+  >
     <Row>
-      <Col
-        style={{
-          backgroundColor: "#FF7F5C",
-          padding: "210px",
-        }}
-      >
-        Footer Container
+      <Col>
+        <div className="footer">
+          <div className="animation-icons">
+            <img className="icon icon-cloud-1" src={Cloud1} alt="" />
+            <img className="icon icon-cloud-2" src={Cloud2} alt="" />
+            <img className="icon icon-cloud-3" src={Cloud3} alt="" />
+            <img className="icon icon-cloud-4" src={Cloud4} alt="" />
+            <img className="icon icon-cloud-5" src={Cloud5} alt="" />
+            <img className="icon icon-cloud-6" src={Cloud6} alt="" />
+
+            <img className="icon icon-building" src={IconBuilding} alt="" />
+          </div>
+          <div className="section-footer-pages">
+            <div className="box-pages">
+              <h1>Pages</h1>
+            </div>
+            <div className="box-pages-link">
+              <ul>
+                <div className="box-link-pages">
+                  <li className="link-home link">
+                    <a Href="#">Home</a>
+                  </li>
+                  <li className="link-how-to-use link">
+                    <a Href="#">How to use</a>
+                  </li>
+                  <li className="link-privacy-policy link">
+                    <a Href="#">Privacy Policy</a>
+                  </li>
+                  <li className="link-terms-and-conditions link">
+                    <a Href="#">Terms and Conditions</a>
+                  </li>
+                </div>
+              </ul>
+            </div>
+          </div>
+
+          <div className="section-footer-download">
+            <div className="box-download">
+              <h1>Download</h1>
+            </div>
+            <div className="box-btn-footer-download">
+              <div className="btn-footer-google-play btn-footer">
+                <a href="#">
+                  <span>
+                    <i className="fab fa-google-play fa-lg"></i>
+                    Google Play
+                  </span>
+                </a>
+              </div>
+              <div className="btn-footer-apple-store btn-footer">
+                <a href="#">
+                  <span>
+                    <i className="fab fa-apple fa-lg"></i>
+                    Apple Store
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footer-bottom-padding">
+          <div className="footer-bottom">
+            <span className="text-footer-bottom">
+              © 2020 Woocer. All rights reserved.
+            </span>
+          </div>
+        </div>
       </Col>
     </Row>
   </Container>
