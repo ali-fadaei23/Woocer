2f19fcc3e7c48a9ac9fac3b7d6e6726aae164028
Ali Fadaei <razorn1375@gmail.com>
2021-06-09 14:14:11 +0430

    fix problem style in 2 component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import createHistory from "history/createBrowserHistory";
 
 // App - css
 import "./App.css";
 
 // Add Components
+
 import Home from "./Components/Home";
 import HowToUse from "./Components/HowToUse";
 import PrivacyPolicy from "./Components/PrivacyPolicy";
+import ScrollToTop from "./Components/Scroll";
+
+const history = createHistory();
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Route exact path="/" component={Home} />
-        <Route path="/how-to-use" component={HowToUse} />
-        <Route path="/privacy-policy" component={PrivacyPolicy} />
+      <Router history={history}>
+        <ScrollToTop />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/how-to-use" component={HowToUse} />
+          <Route path="/privacy-policy" component={PrivacyPolicy} />
+        </Switch>
       </Router>
     );
   }
